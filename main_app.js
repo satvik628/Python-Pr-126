ec156aa969cb2abcaa426ab229a677f9b93cdc2e
satvik628 <71756957+satvik628@users.noreply.github.com>
2021-08-12 08:28:11 +0530

    Main app file codes for launching app . Codes of app.js

--- main_app.js
+++ main_app.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import PickImage from "./screens/react-native-predictor.js";
+import PickImage from "./screens/react-native-predictor-app.js";
 
 export default class App extends React.Component {
   render() {
